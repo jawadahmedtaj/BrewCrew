88b6758efca4d5a2642de87f2ff871f40d06c8b4
Jawad Ahmed <jawadahmedtaj@gmail.com>
2020-05-06 16:05:03 +0200

    Register with firebase completed with valdiation

--- lib/screens/auth/register.dart
+++ lib/screens/auth/register.dart
@@ -11,9 +11,11 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -36,12 +38,14 @@ class _RegisterState extends State<Register> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
               SizedBox(
                 height: 20,
               ),
               TextFormField(
+                validator: (val) => val.isEmpty ? 'Enter an email' : null,
                 autofocus: true,
                 onChanged: (val) {
                   setState(() => email = val);
@@ -51,6 +55,8 @@ class _RegisterState extends State<Register> {
                 height: 20,
               ),
               TextFormField(
+                validator: (val) =>
+                    val.length < 6 ? 'Enter a password 6+ chars long' : null,
                 obscureText: true,
                 onChanged: (val) {
                   setState(() => password = val);
@@ -61,15 +67,29 @@ class _RegisterState extends State<Register> {
               ),
               RaisedButton(
                 onPressed: () async {
-                  print(email);
-                  print(password);
+                  if (_formKey.currentState.validate()) {
+                    dynamic result = await _auth.registerWithEmailAndPassword(
+                        email, password);
+                    if (result == null) {
+                      setState(() {
+                        error = "Please supply a valid email";
+                      });
+                    }
+                  }
                 },
                 color: Colors.pink[400],
                 child: Text(
                   "Register",
                   style: TextStyle(color: Colors.white),
                 ),
-              )
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                error,
+                style: TextStyle(color: Colors.red, fontSize: 14),
+              ),
             ],
           ),
         ),
