ca39997c04ad1083d78f1e31468d22aa979f3727
Jawad Ahmed <jawadahmedtaj@gmail.com>
2020-05-08 15:24:24 +0200

    firestore connected

---
+++ lib/services/database.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+
+  // collection reference 
+  final CollectionReference brewCollection = Firestore.instance.collection('brews');
+
+  
+
+
+}
