eacd3de2f82518dd580df039c559f7186af818bd
Jawad Ahmed <jawadahmedtaj@gmail.com>
2020-05-13 15:58:43 +0200

    enabled editing of data

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -11,14 +11,13 @@ class BrewList extends StatefulWidget {
 class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
-    final brews = Provider.of<List<Brew>>(context);
+    final brews = Provider.of<List<Brew>>(context) ?? [];
 
-    
     return ListView.builder(
       itemCount: brews.length,
       itemBuilder: (context, index) {
         return BrewTile(brew: brews[index]);
       },
-      );
+    );
   }
 }
